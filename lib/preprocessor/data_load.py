c9d2362d96a7702c943a99aaf5f111f221c89f01
Vikulin Vsevolod <v.vikulin@physics.msu.ru>
2017-03-27 10:52:52 +0300

    Add 5 report

--- lib/preprocessor/data_load.py
+++ lib/preprocessor/data_load.py
@@ -5,8 +5,12 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import scipy.io.wavfile
 from data.signal_data import Signal
-
-
+import matplotlib.pyplot as plt
+import seaborn
+#from scipy import signal
+seaborn.set(style="white")
+import numpy as np
+from scipy.fftpack import fft, ifft
 class LoadSignals():
     """Simple class for data loading"""
 
@@ -24,11 +28,41 @@ class LoadSignals():
             full_file_name = data_path +  '/' + filename
             signal_id = (int) (filename.split('-')[0])
             rate_data = scipy.io.wavfile.read(full_file_name)
-            all_signals.append(Signal(signal_id, class_id, rate_data[1], rate_data[0]))
+            all_signals.append(Signal(signal_id, class_id, a_g_filter(rate_data[1],rate_data[0]), rate_data[0]))
         print ('Load ', len(all_signals), ' signals from ', data_path)
         return all_signals
 
 
+def plot_signal(test_signal):
+    plt.plot(test_signal)
+    plt.show()
+
+def high_filter(data, sample_rate):
+    f_signal = fft(data)
+    l = round(len(f_signal)*50/sample_rate);
+    cut_f_signal = f_signal.copy()
+    cut_f_signal[l:len(f_signal)-1] = 0
+    cut_signal = np.real(ifft(cut_f_signal))
+    return cut_signal
+
+def low_filter(data, sample_rate):
+    f_signal = fft(data)
+    l = round(len(f_signal)*5/sample_rate);
+    cut_f_signal = f_signal.copy()
+    cut_f_signal[0:l+1] = 0
+    cut_f_signal[len(f_signal) + 1 - l :] = 0
+    cut_signal = np.real(ifft(cut_f_signal))
+    return cut_signal
+
+def a_g_filter(data, sample_rate):
+    temp = high_filter(data,sample_rate)
+    new =  low_filter(temp, sample_rate)
+    return new.astype('int16')
+
 if __name__ == "__main__":
     test_load_singnal = LoadSignals()
-    test_load_singnal.load_signals('/home/vsevolod/IBS_data/IBS_true', 2)
+    all_signals = test_load_singnal.load_signals('/home/vsevolod/IBS_data/IBS_false', 2)
+    test_signal = all_signals[3].get_feature()['data']
+    sample_rate = all_signals[3].get_feature()['anything']
+    plot_signal(test_signal)
+    plot_signal(a_g_filter(test_signal, sample_rate))
